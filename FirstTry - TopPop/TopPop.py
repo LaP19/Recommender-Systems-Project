b1891bda22c2c48989fd6fc09843070fac92e15f
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-11-20 09:32:03 +0100

    Added useful material from github of the course

---
+++ FirstTry - TopPop/TopPop.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import scipy.sparse as sps
+from Data_manager.split_functions.split_train_validation_random_holdout import split_train_in_two_percentage_global_sample
+from Evaluation.Evaluator import EvaluatorHoldout
+
