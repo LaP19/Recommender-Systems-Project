94bcbc079e874cc12f4cef0a5a4bac7f5962da1f
Valeria <valeria.amato@mail.polimi.it>
2022-12-28 03:45:12 +0100

    skopt package needed - to install
    conda did not found

--- Experimental Algorithms/functions.py
+++ Experimental Algorithms/functions.py
@@ -1,4 +1,6 @@
 import Data_manager.split_functions.split_train_validation_random_holdout as split
+import numpy as np
+
 
 """
 @author: Valeria Amato
@@ -24,3 +26,30 @@ def split_train_in_three_percetanges(URM_all, user_wise, validation_percetage, t
         URM_validation, URM_test = split.split_train_in_two_percentage_global_sample(URM_rest, real_val_percetage)
 
     return URM_train, URM_validation, URM_test
+
+def precision(recommended_items, relevant_items):
+    
+    is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
+    
+    precision_score = np.sum(is_relevant, dtype=np.float32) / len(is_relevant)
+    
+    return precision_score
+
+def recall(recommended_items, relevant_items):
+    
+    is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
+    
+    recall_score = np.sum(is_relevant, dtype=np.float32) / relevant_items.shape[0]
+    
+    return recall_score
+
+def AP(recommended_items, relevant_items):
+   
+    is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
+    
+    # Cumulative sum: precision at 1, at 2, at 3 ...
+    p_at_k = is_relevant * np.cumsum(is_relevant, dtype=np.float32) / (1 + np.arange(is_relevant.shape[0]))
+    
+    ap_score = np.sum(p_at_k) / np.min([relevant_items.shape[0], is_relevant.shape[0]])
+
+    return ap_score
